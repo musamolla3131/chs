d4ca47e4cc7050f1241d9bd70303be85f567600d
Musa Molla <musamolla3131@gmail.com>
2022-11-23 03:44:26 +0600

    Autonication Start

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import CreateAnAccount from './Login/CreateAnAccount';
-
-
+import ForgotPasword from './Login/ForgotPasword';
+import Login from './Login/Login';
 function App() {
   return (
     <div>
+
+      {/* <ForgotPasword /> */}
       {/* <Login /> */}
       <CreateAnAccount />
-      {/* <ForgotPasword /> */}
     </div>
   );
 }
