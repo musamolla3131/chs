9a4897415af7e6798043ce5934b40fcdf864d074
Musa Molla <musamolla3131@gmail.com>
2022-12-03 03:47:06 +0600

    Icon Coustomazaton

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import './App.css';
-import CreateAnAccount from './Login/CreateAnAccount';
-import ForgotPasword from './Login/ForgotPasword';
 import Login from './Login/Login';
 function App() {
   return (
     <div>
 
-      {/* <ForgotPasword /> */}
-      {/* <Login /> */}
-      <CreateAnAccount />
+      <Login />
     </div>
   );
 }
